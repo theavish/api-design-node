a3f09d6fc1e39a779ecf9123a865f6e3ec644214
Avi Samloff <avi.samloff@gmail.com>
2016-03-14 12:56:33 -0700

    completes step 1

--- server.js
+++ server.js
@@ -2,4 +2,21 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+var jsonData = { count: 12, message: 'hey' };
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html', function(error) {
+    if (error) { res.status(500).send(error); }
+  });
+});
+
+app.get('/data', function(req, res) {
+  res.json(jsonData);
+});
+
+app.listen(3000, function() {
+  console.log('server running');
+});
